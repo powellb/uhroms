ccddd58c9a61112b0d823e525bf3e0c63fb804d5
Brian Powell <powellb@hawaii.edu>
2011-05-20 13:43:00 -1000

    Update to nanomoles rather than millimoles

--- ROMS/Nonlinear/Biology/entero_inp.h
+++ ROMS/Nonlinear/Biology/entero_inp.h
@@ -287,7 +287,7 @@
      &            'Number of iterations for nonlinear convergence.'
 #ifdef ANA_BIOLOGY
             WRITE (out,70) BioIni(iEntero,ng), 'BioIni(iEntero)',       &
-     &            'Enterococcus initial concentration (mmol/m3).'
+     &            'Enterococcus initial concentration (nmol/m3).'
 #endif
             WRITE (out,80) PARfracUV(ng), 'PARfracUV',                  &
      &            'Fraction of shortwave radiation that is',            &
--- ROMS/Nonlinear/Biology/entero_mod.h
+++ ROMS/Nonlinear/Biology/entero_mod.h
@@ -35,7 +35,7 @@
       real(r8), allocatable :: BioIni(:,:)
 #endif
       real(r8), dimension(Ngrids) :: AttSWUV, AttSWBlue  ! 1/m
-      real(r8), dimension(Ngrids) :: Ent_DecayUV, Ent_GrowthBlue ! mmol/day
+      real(r8), dimension(Ngrids) :: Ent_DecayUV, Ent_GrowthBlue ! nmol/day
       real(r8), dimension(Ngrids) :: PARfracUV, PARfracBlue ! nondimensional
       real(r8), dimension(Ngrids) :: wEntero         ! m/day
 #ifdef TANGENT
