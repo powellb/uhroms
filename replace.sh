fe417b90801583f85c984ca069aee27a6eeffb02
Brian Powell <powellb@hawaii.edu>
2018-01-09 14:29:54 -1000

    remove temporary script

--- replace.sh
+++
@@ -1,7 +0,0 @@
-for d in Compilers Data Lib Master ROMS User Waves; do
-    echo "Replace ${d}"
-    git rm -r ${d}
-    cp -r ../trunk/${d} .
-    git add ${d}
-done
-git commit -m "Update to latest ROMS trunk"    
